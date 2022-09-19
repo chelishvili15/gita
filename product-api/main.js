287f487cb4ea038c8b8443be84e53d0443b66359
chelishvili15 <chelo@redberry.ge>
2022-09-19 12:55:27 +0400

    home-work1: export product(module)

--- product-api/main.js
+++ product-api/main.js
@@ -3,28 +3,7 @@ const path = require("path");
 
 const app = express();
 
-const products = [
-  {
-    id: 1,
-    product: "Apple",
-    kind: "Fruit",
-  },
-  {
-    id: 2,
-    product: "Orange",
-    kind: "Citrus",
-  },
-  {
-    id: 3,
-    product: "Cherry",
-    kind: "Fruit",
-  },
-  {
-    id: 4,
-    product: "Banana",
-    kind: "Fruit",
-  },
-];
+const products = require('./products')
 
 app.use(express.json());
 
--- product-api/products.js
+++ product-api/products.js
@@ -1,20 +1,24 @@
 const products = [
-    { 
-        id: 1,
-        product: 'Apple'
-    },
-    { 
-        id: 2,
-        product: 'Orange'
-    },
-    { 
-        id: 3,
-        product: 'Cherry'
-    },
-    { 
-        id: 4,
-        product: 'Banana'
-    },
+  {
+    id: 1,
+    product: "Apple",
+    kind: "Fruit",
+  },
+  {
+    id: 2,
+    product: "Orange",
+    kind: "Citrus",
+  },
+  {
+    id: 3,
+    product: "Cherry",
+    kind: "Fruit",
+  },
+  {
+    id: 4,
+    product: "Banana",
+    kind: "Fruit",
+  },
 ];
 
-module.exports = { products }
+module.exports = products 
